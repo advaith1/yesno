03abeaefe30fa951b34268bfc9b1d3a09e69116a
Glitch (moosic2) <none>
2018-12-23 21:05:39 +0000

    ☔️💈 Checkpoint ./commands/stats.js:622795/1237 ./commands/close.js:622795/22

---
+++ commands/stats.js
@@ -0,0 +1,32 @@
+const Spark = require("sparkbots")
+const Command = Spark.command("stats")
+Command.setLevel(0)
+Command.allowDms(true)
+Command.setDescription('close a poll')
+module.exports = Command
+
+const {db} = require('/app/db.js')
+
+Command.code = async (client, message) => {
+    
+  const doc = db.collection('polls').doc(message.channel.id)
+    
+  const docx = await doc.get()
+  
+  if(!docx.data()) return message.channel.send('Looks like there isn\'t a poll currently open.')
+    
+  if(!docx.data().message) return message.channel.send('Looks like there isn\'t a poll currently open.')
+  
+  await message.channel.messages.fetch(docx.data().message)
+  
+  const msg = await await message.channel.messages.fetch(docx.data().message)
+  
+  await message.channel.send({
+      "embed": {
+        "title": `Poll Closed: ${docx.data().q}`,
+        "description": `Yes: ${msg.reactions.get('526209014254665759').count-1}
+No: ${msg.reactions.get('526209037361086526').count-1}`
+      }
+    })
+  
+}
