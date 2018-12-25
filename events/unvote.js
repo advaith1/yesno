f6500034e0a3d54b213736ddb932310eb26cbe9f
Glitch (moosic2) <none>
2018-12-25 01:07:12 +0000

    🚶🎥 Checkpoint ./events/unvote.js:622795/568 ./commands/help.js:622795/737

--- events/unvote.js
+++ events/unvote.js
@@ -1,6 +1,6 @@
 const spark = require("sparkbots")
-const event = spark.event("vote")
-event.setEvent("messageReactionAdd")
+const event = spark.event("unvote")
+event.setEvent("messageReactionRemove")
 
 const {db} = require('/app/db.js')
 
@@ -21,13 +21,7 @@ event.code = async (client, reaction, user) => {
     
     if(!reaction.message.reactions.get('526209037361086526')) return reaction.message.channel.send('Looks like this poll does not have a No reaction on it, manually add one to fix.')
     
-    if(reaction.message.reactions.get('526209037361086526').users.get(user.id)) {
-      
-      reaction.message.reactions.get('526209037361086526').users.remove(user.id)
-      
-      reaction.message.channel.send(`${user.tag} changed their vote from No to Yes`)
-      
-    } else reaction.message.channel.send(`${user.tag} voted Yes`)
+    if(!reaction.message.reactions.get('526209037361086526').users.get(user.id)) reaction.message.channel.send(`${user.tag} removed their vote`)
   
   }
   
@@ -41,13 +35,7 @@ event.code = async (client, reaction, user) => {
     
     if(!reaction.message.reactions.get('526209014254665759')) return reaction.message.channel.send('Looks like this poll does not have a Yes reaction on it, manually add one to fix.')
     
-    if(reaction.message.reactions.get('526209014254665759').users.get(user.id)) {
-      
-      reaction.message.reactions.get('526209014254665759').users.remove(user.id)
-      
-      reaction.message.channel.send(`${user.tag} changed their vote from Yes to No`)
-      
-    } else reaction.message.channel.send(`${user.tag} voted No`)
+    if(!reaction.message.reactions.get('526209014254665759').users.get(user.id)) reaction.message.channel.send(`${user.tag} removed their vote`)
   
   }
   
