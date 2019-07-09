d53afc09e0e65d1d780b22deaff6b678f511ae8f
Glitch (moosic2) <none>
2019-07-09 01:17:10 +0000

    💴🐧 Checkpoint ./functions/engines/botlists.js:622795/1523

--- functions/engines/botlists.js
+++ functions/engines/botlists.js
@@ -8,24 +8,10 @@ const fetch = require('jaczfetch')
 Engine.code = (client, message) => {
     
   blapi.handle(client, {
-    'botlist.space': process.env.blspace,
-    'botsfordiscord.com': process.env.BFD_TOKEN,
-    'discord.boats': process.env.dboats,
-    'discordboats.club': process.env.dbc,
-    // Currently not listed on dbi
-    // discordbotlist.com is below
-    'discordbotlist.xyz': process.env.dblxyz,
-    'discordapps.dev': process.env.ls,
-    'discordbotreviews.xyz': process.env.dbr,
-    'discordbot.world': process.env.dbw,
-    'discord.bots.gg': process.env.dbgg,
-    //'discordbotslist.com': process.env.dbotslist,   Site doe
+    'botlist.space': process.env.bls,
     'discordbots.group': process.env.dbg,
-    // Not listed on discord services yet
     'discordsbestbots.xyz': process.env.dbb,
-    // DEL is down so i cant get api key
-    'divinediscordbots.com': process.env.ddbl,
-    'discordbots.org': process.env.dbl // blapi should manually support dbl now
+    'discordbots.org': process.env.dbl
   }, 1)
   
   // Bots On Discord has a ratelimit of 2 mins
