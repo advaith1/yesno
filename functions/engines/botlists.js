d84db911fbcbf4ebdcf120b3d2ae20453ee6662d
Glitch (yesnobot) <none>
2019-07-11 17:49:23 +0000

    🛥👭 Checkpoint ./functions/engines/botlists.js:622795/252

--- functions/engines/botlists.js
+++ functions/engines/botlists.js
@@ -7,24 +7,29 @@ const fetch = require('jaczfetch')
 
 Engine.code = (client, message) => {
       
-  blapi.handle(client, {
-    'botlist.space': process.env.bls,
-    'discordbots.group': process.env.dbg,
-    'discordsbestbots.xyz': process.env.dbb,
-    'discordbots.org': process.env.dbl
-  }, 3)
-  
-  // Bots On Discord has a ratelimit of 2 mins
-  blapi.handle(client, {
-    'bots.ondiscord.xyz': process.env.bod
-  }, 2)
-  
-  // discordbotlist.com accepts more data
-  setInterval(() => {
-      fetch.post(`https://discordbotlist.com/api/bots/${client.user.id}/stats`)
-        .set('Authorization', 'Bot '+process.env.dblc)
-        .send({ guilds: client.guilds.size, users: client.users.size })
-        .catch(error => console.error(error))
-  }, 60000)
+  try {
+    blapi.handle(client, {
+      'botlist.space': process.env.bls,
+      'discordbots.group': process.env.dbg,
+      'discordsbestbots.xyz': process.env.dbb,
+      'discordbots.org': process.env.dbl
+    }, 3)
+    
+    // Bots On Discord has a ratelimit of 2 mins
+    blapi.handle(client, {
+      'bots.ondiscord.xyz': process.env.bod
+    }, 2)
+    
+    // discordbotlist.com accepts more data
+    setInterval(() => {
+        fetch.post(`https://discordbotlist.com/api/bots/${client.user.id}/stats`)
+          .set('Authorization', 'Bot '+process.env.dblc)
+          .send({ guilds: client.guilds.size, users: client.users.size })
+          .catch(error => console.error(error))
+    }, 60000)
+
+  } catch (e) {
+    console.error(e)
+  }
   
 }
