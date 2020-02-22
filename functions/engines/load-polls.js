686818d89aff2638ba5a2a77a8d39db9975959ab
Glitch (yesnobot) <none>
2020-02-22 02:59:25 +0000

    🐽🌧 Checkpoint ./functions/engines/load-polls.js:622795/765

--- functions/engines/load-polls.js
+++ functions/engines/load-polls.js
@@ -9,10 +9,20 @@ Engine.code = (client, message) => {
   db.collection('polls').get().then((snapshot) => {
       snapshot.forEach((doc) => {
         
+        // Cancel if channel isn't found
         if(!client.channels.resolve(doc.id)) return
         
-        client.channels.resolve(doc.id).messages.fetch(doc.data().message).catch(e => {})
-        
+        // Fetch message
+        client.channels.resolve(doc.id).messages.fetch(doc.data().message).catch(e => console.error(e))
+
+        // Cancel if message isn't found
+        if(!client.channels.resolve(doc.id).messages.resolve(doc.data().message)) return
+
+        // Fetch yes reactions
+        client.channels.resolve(doc.id).messages.resolve(doc.data().message).reactions.resolve('526209014254665759').users.fetch()
+
+        // Fetch no reactions
+        client.channels.resolve(doc.id).messages.resolve(doc.data().message).reactions.resolve('526209037361086526').users.fetch()
       })
   })
   
