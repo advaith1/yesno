7bacf82b19dd03d8398fb69b90000ba70d0937b3
Glitch (yesnobot) <none>
2020-02-22 03:11:29 +0000

    📙🏮 Checkpoint ./functions/engines/load-polls.js:622795/380

--- functions/engines/load-polls.js
+++ functions/engines/load-polls.js
@@ -7,22 +7,22 @@ const {db} = require('/app/db.js')
 Engine.code = (client, message) => {
 
   db.collection('polls').get().then((snapshot) => {
-      snapshot.forEach((doc) => {
+      snapshot.forEach(async (doc) => {
         
         // Cancel if channel isn't found
         if(!client.channels.resolve(doc.id)) return
         
         // Fetch message
-        client.channels.resolve(doc.id).messages.fetch(doc.data().message).catch(e => console.error(e))
+        await client.channels.resolve(doc.id).messages.fetch(doc.data().message).catch(e => console.error(e))
 
         // Cancel if message isn't found
         if(!client.channels.resolve(doc.id).messages.resolve(doc.data().message)) return
 
         // Fetch yes reactions
-        client.channels.resolve(doc.id).messages.resolve(doc.data().message).reactions.resolve('526209014254665759').users.fetch()
+        client.channels.resolve(doc.id).messages.resolve(doc.data().message).reactions.resolve('526209014254665759').users.fetch().catch(e => console.error(e))
 
         // Fetch no reactions
-        client.channels.resolve(doc.id).messages.resolve(doc.data().message).reactions.resolve('526209037361086526').users.fetch()
+        client.channels.resolve(doc.id).messages.resolve(doc.data().message).reactions.resolve('526209037361086526').users.fetch().catch(e => console.error(e))
       })
   })
   
